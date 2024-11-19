287ad161eb06e55df31e2225220a9bb96e75eee4
el quero <92737938+el-quero@users.noreply.github.com>
2024-11-20 00:10:58 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('reservation-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Reservasi Anda telah berhasil dikirim. Terima kasih!');
+});
